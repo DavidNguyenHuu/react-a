8797fa8f05e926e30017ad4499b77c0e748907d8
David Nguyen-Huu <nguye@DESKTOP-6OB9G0V>
2021-05-27 20:43:02 -0400

    init

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Todo from "./components/Todo";
 function App() {
   return (
     <div>
-      <h1>David's personal website</h1>
+      <h1>David</h1>
       <Todo text="Learn react" />
       <Todo text="Master React" />
       <Todo text="Explore the full react course"></Todo>
